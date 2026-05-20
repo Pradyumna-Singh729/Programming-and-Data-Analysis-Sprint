a34c011c3e535eeb7fb7e18cef7324d0551a6f99
Pradyumna-Singh729 <sattire729@gmail.com>
2026-05-20 15:10:24 +0530

    Completed Upto 2-1

---
+++ Python-Crash-Course/02_Variables_and_Simple_Data_Types/practice_problems/try_it_yourself_01_2-2.py
@@ -0,0 +1,5 @@
+# 2-2. Simple Messages: Assign a message to a variable, and then print that message. Then change the value of the variable to a new message, and print the new message.
+furina = "Hello, I'm Furina!"
+print(furina)
+furina = "I am the Hydro Archon of Fontaine!"
+print(furina)
