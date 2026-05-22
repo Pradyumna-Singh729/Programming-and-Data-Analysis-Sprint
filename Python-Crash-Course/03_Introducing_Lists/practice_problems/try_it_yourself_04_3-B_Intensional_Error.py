ee07f17a843990d0e394671013631f694fbbfb0d
Pradyumna-Singh729 <sattire729@gmail.com>
2026-05-22 18:42:56 +0530

    Completed Chapter 3 of Python Crash Course, Introducing Lists

---
+++ Python-Crash-Course/03_Introducing_Lists/practice_problems/try_it_yourself_04_3-B_Intensional_Error.py
@@ -0,0 +1,3 @@
+# 3-11 Intentional Error: If you haven't recieved an index error in one of your programmes yet, try to make one happen. Change an index in one of your programs to produce an index error.
+motorcycles = ['honda', 'yamaha', 'suzuki']
+first_owned = motorcycles.pop(4)
