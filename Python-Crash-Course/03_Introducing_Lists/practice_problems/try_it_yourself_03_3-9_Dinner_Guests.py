dadb9c8616bef534445da2415dc006ca1f2dd5fa
Pradyumna-Singh729 <sattire729@gmail.com>
2026-05-22 15:07:12 +0530

    Progressed upto the 3rd problem set of 3rd chapter of Python Crash Course

---
+++ Python-Crash-Course/03_Introducing_Lists/practice_problems/try_it_yourself_03_3-9_Dinner_Guests.py
@@ -0,0 +1,8 @@
+# 3-9 Dinner Guests: Start with your program from Exercise 3-4 or Exercise 3-5. Add a print() call at the end of your program indicating the number of people you are inviting to dinner.
+invited = ["Furina", 'King', 'Euclid', 'Euler']
+print(f'Hello, {invited[0]}, you are invited to dinner!')
+print(f'Hello, {invited[-3]}, you are invited to dinner!')
+print(f'Hello, {invited[2]}, you are invited to dinner!')
+print(f'Hello, {invited[-1]}, you are invited to dinner!')
+number =len(invited)
+print(f"The number of people invited to dinner is {number}.")
