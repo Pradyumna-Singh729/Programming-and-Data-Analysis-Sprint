3150a6d47a84cbcc2308d24076ac0b6b804b9479
Pradyumna-Singh729 <sattire729@gmail.com>
2026-05-24 19:31:57 +0530

    Completed chapter 4 of python crash course till the 3rd problem set

---
+++ Python-Crash-Course/04_Working_with_Lists/sandbox/19-looping_through_a_slice.py
@@ -0,0 +1,5 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print("here are the first three players in my team :")
+for player in players[:3]:
+    print(player.title())
+    
---
+++ Python-Crash-Course/04_Working_with_Lists/sandbox/22-copying_a_list_3.py
@@ -0,0 +1,9 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+# This doesnn't work
+friend_foods = my_foods
+my_foods.append("cannoli")
+friend_foods.append('ice cream')
+print('My favorite foods are:')
+print(my_foods)
+print("\nMy friends's favorite foods are:")
+print(friend_foods)
