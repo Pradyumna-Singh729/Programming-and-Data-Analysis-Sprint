daad5512c914e8d7a494a852fc0e242dedf86918
Pradyumna-Singh729 <sattire729@gmail.com>
2026-05-27 16:43:35 +0530

    Understood using if statements with lists

---
+++ Python-Crash-Course/05_If_Statements/sandbox/20-checking_for_special_items_1.py
@@ -0,0 +1,6 @@
+requested_toppings = ['mushroom', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    print(f"Added {requested_topping}")
+
+print("\nFinished making your pizza")
---
+++ Python-Crash-Course/05_If_Statements/sandbox/21-checking_for_special_items_2.py
@@ -0,0 +1,9 @@
+requested_toppings = ['mushroom', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping == "green peppers":
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
+    
+print("\nFinished making your pizza.")
